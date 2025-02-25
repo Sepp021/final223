17dbc0e06aaf019aebc4fd74b6ee225bf0615490
jmfyrht <mka@A34PC3>
2025-02-25 12:35:50 +0200

    rgshbf

--- final1/Program.cs
+++ final1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Zdorov! Створення, редагування, видалення словників\r\n\tДодавання, редагування, видалення слів в словнику та їх перекладів\r\n\tПошук слів та перекладів\r\n\tЗберігати словники в файл\r\n ");
