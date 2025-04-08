2bdcc01bf8e61d2f9d9e00bdf24db23ef725866d
mka <mka@A34PC3>
2025-04-08 14:37:01 +0300

    iuwfgsp;

--- final1/Program.cs
+++ final1/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Zdorov! Створення, редагування, видалення словників\r\n\tДодавання, редагування, видалення слів в словнику та їх перекладів\r\n\tПошук слів та перекладів\r\n\tЗберігати словники в файл\r\n ");
+/*8. ------------------- Змагання 
+Створити програму для обліку результатів змагань з легкої атлетики(чи ін.)
+   Вивід учасників змагань(спортсмен, тренер, країна)
+   Додавання(вилучення, редагування) спортсмена у(з) базу
+   Вивід видів змагань(100м, 3км, стрибки та ін.)
+   Додавання(вилучення, редагування) виду змагань
+   Облік результатів змагань у певному виді
+   Збереження у файлі(завантаження з файлу) переліку спортсменів
+   Збереження у файлі(завантаження з файлу) переліку видів змагань	
+   Збереження у файлі(завантаження з файлу) результатів змагань по датах
+   Підведення підсумків (визначення переможців у певному виді змагання)*/
+
+using System.Text.Json;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8; // для укр. тексту
+
+Console.WriteLine("------------ Welcome to Competition App (8) ------------");
+
+Console.WriteLine("----- MENU -----");
+Console.WriteLine("1. Open page");
+Console.WriteLine("2. Search word");
+Console.WriteLine("3. Discription word");
+Console.WriteLine("4. Meaning word");
+Console.WriteLine("5. Salamualekum");
+Console.WriteLine("6. Malekum asalam");
+Console.WriteLine("7. Word history");
+Console.WriteLine("8. Word lenguage");
+Console.WriteLine("9. Chinazes");
+
+
